8e86033bb99e3ed981ac3985f1cfe2905e2f4ec5
dgoceva <44052125+dgoceva@users.noreply.github.com>
2023-12-06 12:56:24 +0200

    Delete RMI directory

--- RMI/MessengerService.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface MessengerService extends Remote {
-
-	public String sendMessage(String clientMessage) throws RemoteException;
-
-	public Message sendMessage(Message clientMessage) throws RemoteException;
-}
--- RMI/RMI/RMIInterface.java
+++
@@ -1,8 +0,0 @@
-package RMI;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RMIInterface extends Remote {
-	
-	public String helloTo(String name) throws RemoteException;
-}
